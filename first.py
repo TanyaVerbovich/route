7df76e732f1b1f4bf53489dd84ac0e235bce0e33
TanyaVerbovich <tanya.verbovich@gmail.com>
2021-12-27 15:42:06 +0300

    added creating db and new page

--- first.py
+++ first.py
@@ -1,14 +1,67 @@
-from flask import Flask, render_template
+from collections import namedtuple
+from flask import Flask, render_template, redirect, url_for, request, g
+import sqlite3
+import os
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsite.db'
+SECRET_KEY = '6nT9Nm6nT9Nm6nT9Nm'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+Message = namedtuple('Message', 'text tag')
+messages = []
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu())
+
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/add message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+    messages.append(Message(text, tag))
+
+    return redirect(url_for('main'))
 
 
 if __name__ == "__main__":
---
+++ sq_db.sql
@@ -0,0 +1,5 @@
+create table if not exists maimenu (
+id integer primary key AUTOINCREMENT,
+title text not null,
+url text not null
+);
